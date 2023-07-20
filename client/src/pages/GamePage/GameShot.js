4acfedef314d63a72b32b0df411da30a6c8ab289
유민국 <acd4548@naver.com>
2023-07-20 12:45:06 +0900

    FEAT: MainPage_07201244

---
+++ client/src/pages/GamePage/GameShot.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>사진촬영 페이지</h1>
+      <p>게임 끝난 후, 사진촬영 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/MyPage/Mypage.js
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>마이페이지 페이지</h1>
+      <p>내 가입 정보 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Home;
