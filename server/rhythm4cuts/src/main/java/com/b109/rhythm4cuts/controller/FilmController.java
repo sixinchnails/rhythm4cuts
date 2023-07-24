81664a099ae540da54d2a96c6b74ea3678b552ec
chlgksdbs <chlgksdbs98@naver.com>
2023-07-24 17:09:45 +0900

    FEAT: film controller, service, repository, dto 생성

---
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/controller/FilmController.java
@@ -0,0 +1,36 @@
+package com.b109.rhythm4cuts.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/film")
+public class FilmController {
+    
+    // 인생네컷의 테두리 개수 (추후 DB에서 count 함수로 값을 끌어올 예정)
+    static final int PHOTO_SIZE = 4;
+
+    // API 1. 인생네컷 사진 촬영
+    @PostMapping("/photo")
+    public static void photoShoot(@RequestBody GameImageDto gameImageDto) {
+
+        // 넘어온 사진 정보를 DB에 저장할 예정
+        filmService.photoShoot(gameImageDto);
+    }
+
+    // API 2. 데일리 방명록 사진 리스트
+    // date: 방명록 조회 날짜 (ex. 2023-07-24)
+    @GetMapping("/photo/{date}")
+    public static void dailyPhotoList(@PathVariable("date") String date) {
+
+        // DB에서 'WHERE create_date LIKE 'date%''와 같은 형태로 검색할 예정
+        filmService.dailyPhotoList(date);
+    }
+
+    // API 3. 인생네컷 테두리 전체 조회
+    @GetMapping("/border")
+    public static void borderList() {
+
+        // DB의 배경 테이블에 존재하는 모든 이미지 파일들을 불러올 예정
+        filmService.borderList();
+    }
+}
---
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/model/service/FilmService.java
@@ -0,0 +1,9 @@
+package com.b109.rhythm4cuts.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FilmService {
+
+
+}
