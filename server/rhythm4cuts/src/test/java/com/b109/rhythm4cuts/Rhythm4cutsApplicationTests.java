ff00192d6b608084fe5d089e2676033cf3627aee
홍유빈 <acrow0330@naver.com>
2023-07-27 12:31:30 +0900

    FIX: test 수정

--- server/rhythm4cuts/src/test/java/com/b109/rhythm4cuts/Rhythm4cutsApplicationTests.java
+++ server/rhythm4cuts/src/test/java/com/b109/rhythm4cuts/Rhythm4cutsApplicationTests.java
@@ -3,7 +3,7 @@ package com.b109.rhythm4cuts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Rhythm4cutsApplicationTests {
 
 	@Test
