0a1fcca237b3de6db858a453c6e5173c55dd1f31
unknown <ghddbqls33@mail.hongik.ac.kr>
2023-08-03 01:34:11 +0900

    FIX: websocket cors 해결

--- server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/controller/StompController.java
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/controller/StompController.java
@@ -5,9 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class StompController {
     private final SimpMessagingTemplate messagingTemplate;
     @MessageMapping(value = "/request")
