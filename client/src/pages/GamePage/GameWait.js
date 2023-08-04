83b472680b90fbbd5a077203bb0516d5d076e754
EUNSEONG LEE <pro30343069@gmail.com>
2023-08-04 09:46:54 +0900

    FEAT: CONNECTION

--- client/src/pages/GamePage/GameWait.js
+++ client/src/pages/GamePage/GameWait.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useEffect } from "react";
 import { Button, Card, Container, Grid, Link as MuiLink } from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -6,13 +7,15 @@ import { toggleReady, fetchToken } from "../../store";
 import Header from "../../components/Game/Header_light";
 import Next from "../../components/Game/NextToPlay";
 import Webcam from '../../components/Game/Webcam';
+import axios from "axios";
+import { getCookie } from "../../utils/cookie";
 
 function GameWait() {
   const { gameSeq } = useParams(); // URL에서 gameSeq 파라미터를 가져옵니다.
   const navigate = useNavigate(); // React Router의 useNavigate hook을 사용하여 페이지 이동을 수행합니다.
   const dispatch = useDispatch(); // Redux의 dispatch 함수를 가져옵니다.
 
-  const token = useSelector(state => state.session.token); // Redux의 상태에서 세션 토큰을 가져옵니다.
+  const token = useSelector((state) => state.session.token); // Redux의 상태에서 세션 토큰을 가져옵니다.
 
   const [isInviteFriendModalOpen, setInviteFriendModalOpen] = React.useState(false); // 친구 초대 모달 상태를 관리하는 상태 변수를 생성합니다.
 
@@ -30,16 +33,54 @@ function GameWait() {
     dispatch(toggleReady(playerId)); // Redux의 toggleReady 액션을 호출하여 플레이어의 준비 상태를 변경합니다.
   };
 
+  //로그인 상태 확인
+  // const [isLogin, setIsLogin] = useState(false);
+  const user_seq="";
+  try {
+    userInfo()
+      .then(res => {
+        if (res.status === 200) {
+          console.log(res.data.user_seq);
+          user_seq = res.data.user_seq;
+        }
+      })
+      .catch(error => {
+        window.alert("로그인을 해주세요!");
+        navigate("/");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+
   useEffect(() => {
     if (gameSeq) {
       dispatch(fetchToken(gameSeq)); // 컴포넌트가 마운트되고 gameSeq가 있을 때 fetchToken 액션을 호출하여 토큰을 가져옵니다.
     }
   }, [dispatch, gameSeq]);
 
+
   useEffect(() => {
     if (token) {
-      // TODO: 토큰이 있을 때 세션 생성 로직 추가
-      // (OpenVidu 세션을 생성하고 웹캠 스트림을 활성화하는 로직 등을 추가할 수 있습니다.)
+      axios.put(
+        "/lobby/enter",
+        {
+          user_seq: user_seq,
+          connection_id: token, // 저장할 연결 토큰
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + getCookie("access"), // 사용자 인증 토큰을 헤더에 넣어서 보냅니다.
+          },
+        }
+      )
+        .then((response) => {
+          // 저장 성공 시의 처리를 추가할 수 있습니다.
+          console.log("연결 토큰이 사용자 DB에 저장되었습니다.");
+        })
+        .catch((error) => {
+          // 저장 실패 시의 처리를 추가할 수 있습니다.
+          console.error("연결 토큰 저장에 실패했습니다.", error);
+        });
     }
   }, [token]);
 
