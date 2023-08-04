0a2b879a43e4a56f6dc1cb6f248f4b747a84ef9d
홍유빈 <acrow0330@naver.com>
2023-08-04 17:28:27 +0900

    FIX: nginx 설정 파일 작성

--- server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/config/DummyDataInitializer.java
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/config/DummyDataInitializer.java
@@ -74,7 +74,7 @@ public class DummyDataInitializer {
         User user2 = new User();
         user2.setName("hyun");
         user2.setNickname("hyun");
-        user2.setEmail("hyunleo89@gmail.com");
+        user2.setEmail("abc@gmail.com");
         user2.setProfileImage(profileImage1);
         user2.setBirthDate(targetDate);
         user2.setPassword(bCryptPasswordEncoder.encode("1234"));
