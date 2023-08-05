f3a6cfbdcff4fc118f9724007ef5d72aefa7062e
홍유빈 <acrow0330@naver.com>
2023-08-06 01:18:48 +0900

    Update client/src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import { CookiesProvider } from "react-cookie";
@@ -13,9 +13,9 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <CookiesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CookiesProvider>
   </Provider>
   // </React.StrictMode>
