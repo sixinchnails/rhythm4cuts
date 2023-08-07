5eb396e9ff5b467e67cb18e1551326bc3aedd2e4
EUNSEONG LEE <pro30343069@gmail.com>
2023-08-07 14:46:35 +0900

    FEAT: MASTER PLZ

--- client/src/apis/userInfo.js
+++ client/src/apis/userInfo.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import { getCookie } from "../utils/cookie";
 
 export const userInfo = async () => {
-   console.log("안녕")
+  console.log("안녕")
   const params = {
     email: getCookie("email"),
   };
   const headers = {
     Authorization: "Bearer " + getCookie("access"),
   };
-  const result = await axios.get("/member/info", {
+  const result = await axios.get("https://i9b109.p.ssafy.io:8443/member/info", {
     params,
     headers,
   });
