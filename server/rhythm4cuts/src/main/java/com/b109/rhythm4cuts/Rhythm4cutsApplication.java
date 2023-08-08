347293fe27d8013d32fdc9b553740c9f7418a527
chlgksdbs <chlgksdbs98@naver.com>
2023-08-08 10:22:30 +0900

    FEAT: add Scheduled

--- server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/Rhythm4cutsApplication.java
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/Rhythm4cutsApplication.java
@@ -2,8 +2,10 @@ package com.b109.rhythm4cuts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Rhythm4cutsApplication {
 
 	public static void main(String[] args) {
