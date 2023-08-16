143ee1ee46af0b9a81f27e80f71a9c8975fcedd6
EUNSEONG LEE <pro30343069@gmail.com>
2023-08-16 12:38:38 +0900

    feat

--- client/src/components/Game/UserVideoComponent.js
+++ client/src/components/Game/UserVideoComponent.js
@@ -92,7 +92,7 @@ const UserVideoComponent = ({ streamManager }) => {
             {/* {getUserInfo().data.user_seq}  */}
             {userOrders[userSeq] ? (
               <>
-                Order : {userOrders[userSeq]}
+                {userOrders[userSeq]} 번째 입니다!
               </>
             ) : ( 
               "Loading..."
