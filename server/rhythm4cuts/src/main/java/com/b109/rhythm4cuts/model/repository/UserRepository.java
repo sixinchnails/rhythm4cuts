899431264c92843d252e036e7ef358931cab94e0
chlgksdbs <chlgksdbs98@naver.com>
2023-08-17 02:33:06 +0900

    FIX: DB error

--- server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/model/repository/UserRepository.java
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/model/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.b109.rhythm4cuts.model.repository;
 
 import com.b109.rhythm4cuts.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
