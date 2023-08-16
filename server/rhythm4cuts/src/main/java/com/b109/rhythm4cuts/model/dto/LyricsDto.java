cf44c604213824b6a737f16e3488b1280aae01b4
chlgksdbs <chlgksdbs98@naver.com>
2023-08-16 13:17:10 +0900

    FEAT: Lyrics API

---
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/model/dto/LyricsDto.java
@@ -0,0 +1,11 @@
+package com.b109.rhythm4cuts.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LyricsDto {
+
+    private int lyricsSeq;
+    private int startTime;
+}
---
+++ server/rhythm4cuts/src/main/java/com/b109/rhythm4cuts/model/service/LyricsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.b109.rhythm4cuts.model.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LyricsServiceImpl implements LyricsService {
+}
