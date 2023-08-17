d97df1ab8de2b31d0227ebae422d1f8a1a532629
EUNSEONG LEE <pro30343069@gmail.com>
2023-08-17 19:47:27 +0900

    feat

---
+++ client/src/components/Common/DeleteAlert.js
@@ -0,0 +1,48 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
+import React from "react";
+
+function LoginAlert({ isOpen, onClose }) {
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.8)", fontFamily: 'Ramche', }}
+    >
+      <div style={{ backgroundColor: "rgba(0, 128, 255, 0.3)", width: "25vw", fontFamily: 'Ramche', }}>
+        <DialogTitle
+          style={{
+            backgroundColor: "rgba(0, 128, 255, 0.2)",
+            display: "flex",
+            justifyContent: "center",
+            fontFamily: 'Ramche',
+          }}
+        >
+          <WarningIcon />
+        </DialogTitle>
+        <DialogContent
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            fontFamily: 'Ramche',
+          }}
+        >
+          <p style={{fontFamily: 'Ramche',}}>로그인 후 이용 가능한 서비스입니다.</p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose} color="primary" autoFocus style={{fontFamily: 'Ramche',}}>
+            확인
+          </Button>
+        </DialogActions>
+      </div>
+    </Dialog>
+  );
+}
+
+export default LoginAlert;
